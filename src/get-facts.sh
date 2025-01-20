7560e5974a7a4d6356c87120b67f689abe02831b
codgician <15964984+codgician@users.noreply.github.com>
2025-01-21 00:03:10 +0800

    hack: mock ipv6 only

--- src/get-facts.sh
+++ src/get-facts.sh
@@ -11,7 +11,7 @@ isKexec=$(if test -f /etc/is_kexec; then echo "y"; else echo "n"; fi)
 isNixos=$isNixos
 isInstaller=$(if [ "$isNixos" = "y" ] && grep -Eq 'VARIANT_ID="?installer"?' /etc/os-release; then echo "y"; else echo "n"; fi)
 isContainer=$(if [ "$(has systemd-detect-virt)" = "y" ]; then systemd-detect-virt --container; else echo "none"; fi)
-hasIpv6Only=$(if [ "$(has ip)" = "n" ] || ip r g 1 >/dev/null 2>/dev/null || ! ip -6 r g :: >/dev/null 2>/dev/null; then echo "n"; else echo "y"; fi)
+hasIpv6Only=y
 hasTar=$(has tar)
 hasCpio=$(has cpio)
 hasSudo=$(has sudo)
